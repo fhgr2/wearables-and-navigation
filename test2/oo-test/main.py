374c935b72bf97752e81ccbbd4ab8819c82dfe49
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2018-04-23 16:38:26 +0200

    Add logging to oo-test/main.py

--- test2/oo-test/main.py
+++ test2/oo-test/main.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
 from helper import Helper
 
 class Main:
     def __init__(self, args, name):
-        self.blub(name)
         helper = Helper()
+        self.__logger = logging.getLogger(__name__)
+        console_handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s') # https://docs.python.org/3/howto/logging-cookbook.html
+        console_handler.setFormatter(formatter)
+        self.__logger.addHandler(console_handler)
+
+        self.blub(name)
 
     def blub(self, asdf):
         print(asdf)
-
+        self.__logger.warn('log-eintrag von Main')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
