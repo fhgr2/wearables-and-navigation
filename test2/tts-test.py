5631b718557b5a66f674881f7c8f50b1dd6e3f7c
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2018-05-18 19:05:18 +0200

    Cosmetic change

--- test2/tts-test.py
+++ test2/tts-test.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
-# sudo apt install python-espeak
-# pip3 install pyttsx3
 
-import pyttsx3;
+import pyttsx3; # sudo apt install python-espeak ; pip3 install pyttsx3
 
 def wait_for_say(text):
     engine = pyttsx3.init();
