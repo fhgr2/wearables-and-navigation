edce714c365e5c9243183b41c533b236cc17d362
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2019-04-10 17:59:10 +0200

    Remove key

--- test2/ors-test.py
+++ test2/ors-test.py
@@ -6,7 +6,7 @@ from openrouteservice.directions import convert
 
 coords = ((9.52864,46.85449),(9.50131,46.84811))
 
-client = openrouteservice.Client(key='***REMOVED***')
+client = openrouteservice.Client(key='yourkeygoeshere')
 routes = directions(client, coords, profile="cycling-safe", instructions="true", bearings=[[40,45]], continue_straight="true", optimized="false") # TODO: add further parameters, see https://openrouteservice-py.readthedocs.io/en/latest/#module-openrouteservice.directions
 
 print("routes=" + str(routes))
